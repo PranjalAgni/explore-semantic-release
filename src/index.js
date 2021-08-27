f22f618dcfdabb0870f48113c4fd2ecebd7b23f6
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-08-27 11:16:32 +0530

    :sparkles: implemented time sort

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 console.log('This is semantic release class');
 console.log('Triggering a new release.');
+
+const timeSort = (array) => {
+  array.forEach((element) => {
+    setTimeout(() => {
+      console.log(element);
+    }, element);
+  });
+};
+
+console.log(timeSort([12, 3, 4, 56]));
